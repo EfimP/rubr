0e39b1cf9b381101c8bc517421607789d1f2af03
aerjiemi <artemiy.hom@gmail.com>
2025-06-11 13:52:48 +0300

    updated schema.sql

--- internal/db/postgres/migrations/schema.sql
+++ internal/db/postgres/migrations/schema.sql
@@ -43,6 +43,10 @@ CREATE TABLE groups_in_disciplines (
     id SERIAL PRIMARY KEY,
     group_id BIGINT NOT NULL,
     discipline_id BIGINT NOT NULL,
+	seminarist_id BIGINT NOT NULL, 
+	assistant_id BIGINT NOT NULL,
+	FOREIGN KEY (seminarist_id) REFERENCES users(id) ON DELETE SET NULL,
+	FOREIGN KEY (assistant_id) REFERENCES users(id) ON DELETE SET NULL,
     FOREIGN KEY (group_id) REFERENCES student_groups(id) ON DELETE CASCADE,
     FOREIGN KEY (discipline_id) REFERENCES disciplines(id) ON DELETE CASCADE
 );
@@ -80,19 +84,12 @@ CREATE TABLE criteria (
     id SERIAL PRIMARY KEY,
     criteria_group_id BIGINT NOT NULL,
     weight BIGINT NOT NULL CHECK (weight >= 0),
-    FOREIGN KEY (criteria_group_id) REFERENCES criteria_groups(id) ON DELETE CASCADE
-);
-
--- 9) criteria_mark_distribution
-CREATE TABLE criteria_mark_distribution (
-    id SERIAL PRIMARY KEY,
-    criteria_id BIGINT NOT NULL,
     comment_000 TEXT,
     comment_025 TEXT,
     comment_050 TEXT,
     comment_075 TEXT,
     comment_100 TEXT,
-    FOREIGN KEY (criteria_id) REFERENCES criteria(id) ON DELETE CASCADE
+    FOREIGN KEY (criteria_group_id) REFERENCES criteria_groups(id) ON DELETE CASCADE
 );
 
 -- Таблица работ студентов
